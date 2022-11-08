083715161e90b65cd86339c37e9e31b814d3deed
bony075 <bony.075@gmail.com>
2022-11-08 12:39:26 +0600

    demo header footer add

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../Layout/Main';
+import Home from '../../Pages/Home/Home/Home';
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <></>,
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element:<Home></Home>
+            }
+        ]
 
 
     }
