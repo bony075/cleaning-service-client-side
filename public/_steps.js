52ae8dae99ac8165308df08db8f598171d3d3c57
Bony <bony.075@gmail.com>
2022-11-09 08:30:03 +0600

    install and config firebase

--- public/_steps.js
+++ public/_steps.js
@@ -2,7 +2,12 @@
 // https://i.ibb.co/ss98Mw2/2.jpg
 // https://i.ibb.co/wBxPZtP/3.jpg
 // https://i.ibb.co/JtrWvP4/4.jpg
-// https://i.ibb.co/HBfyW7J/5.jpg
+// https://i.ibb.co/HBfyW7J/5.'REACT_APP_apiKey=AIzaSyDv7WJxSecHtuaFlI8Gmwv1jhDFL3SHuYY
+// REACT_APP_authDomain = cleaning - service - b60b5.firebaseapp.com
+// REACT_APP_projectId = cleaning - service - b60b5
+// REACT_APP_storageBucket = cleaning - service - b60b5.appspot.com
+// REACT_APP_messagingSenderId = 989165865606
+// REACT_APP_appId = 1: 989165865606: web:fdce66a10eeecc4dab9f07'jpg
 // https://i.ibb.co/7N0Y7KP/6.jpg
 // https://i.ibb.co/Y3kh56S/7.jpg
 // https://i.ibb.co/G32tjZ9/8.jpg
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 
 
 const SignUp = () => {
-  
+
     const handleSignUp = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
 
-      
+
     }
 
     return (
