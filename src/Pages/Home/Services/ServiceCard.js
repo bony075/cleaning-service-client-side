09ca1b3b5f4df7c9e80da4e9bf1970e135124bbe
bony075 <bony.075@gmail.com>
2022-11-09 16:51:18 +0600

    add 404 page

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,18 +1,24 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
+
 
 const ServiceCard = ({ service }) => {
   const {_id,img,title } = service;
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src={img} alt="Shoes" /></figure>
+        <figure><img src={img} alt="service" /></figure>
+        
   <div className="card-body">
           <h2 className="card-title">{ title}</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>If a dog chews shoes whose shoes does he choose?
+           
+    </p>
     <div className="card-actions justify-end">
             <Link to={`/servicedetails/${_id}`}>
               <button className="btn btn-primary">Buy Now</button>
             </Link>
+            
     </div>
   </div>
 </div>
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../../Layout/Main';
 import Home from '../../Pages/Home/Home/Home';
 import Login from '../../Pages/Login/Login';
+import PageNotFound from '../../Pages/PageNotFound/PageNotFound';
 import ServiceDetails from '../../Pages/ServiceDetails/ServiceDetails';
 import SignUp from '../../Pages/SignUp/SignUp';
 const router = createBrowserRouter([
@@ -35,6 +36,10 @@ const router = createBrowserRouter([
         ]
 
 
+    },
+     {
+        path: "*",
+        element: < PageNotFound />
     }
 ])
 export default router;
