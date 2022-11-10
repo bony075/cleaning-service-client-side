b8b025824f06cd44962c29b18ed5f06a1637d164
bony075 <bony.075@gmail.com>
2022-11-10 17:29:07 +0600

    install react icon

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -48,37 +48,68 @@ const Login = () => {
         
     }
     return (
-        <div className="hero w-full my-20">
-            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
-                <div className="text-center lg:text-left">
-                    <img className='w-3/4' src={"img"} alt="" />
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
-                    <h1 className="text-5xl text-center font-bold">Login</h1>
-                    <form onSubmit={handleLogin} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <input className="btn btn-primary" type="submit" value="Login" />
-                        </div>
-                    </form>
-                    <p className='text-center'>New to cleaning service <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
-                </div>
+      <div className="hero w-full my-20">
+        <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+          <div className="text-center lg:text-left">
+            <img className="w-3/4" src={"img"} alt="" />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+            <h1 className="text-5xl text-center font-bold">Login</h1>
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
+            <div className="btn-group btn-group-vertical">
+              <button onClick={handelGoogleSignIn} className="btn mb-4 ">
+                <FaGoogle className="mr-2 text-green-400"></FaGoogle> Google
+                Sign in
+              </button>
+
+              <button onClick={handelGithubSignIn} className="btn">
+                <FaGithub className="mr-2 "></FaGithub> GitHub Sign in
+              </button>
             </div>
+            <p className="text-center">
+              New to cleaning service{" "}
+              <Link className="text-orange-600 font-bold" to="/signup">
+                Sign Up
+              </Link>{" "}
+            </p>
+          </div>
         </div>
+      </div>
     );
 };
 
